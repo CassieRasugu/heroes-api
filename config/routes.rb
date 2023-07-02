71eddd6a00e17669e9bd0df60d41bac70c7a9fa4
Cassie <cassierasugu@gmail.com>
2023-07-02 20:15:48 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :heroes, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
